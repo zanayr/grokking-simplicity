66c4e6107b55dbc40bcabbfb36901b7f086a60d7
Ryan Fickenscher <rsficken@gmail.com>
2022-09-28 00:55:07 -0700

    chapter 10, type safety and clean up

--- chapter_10/chapter10.js
+++ chapter_10/chapter10.js
@@ -5,17 +5,40 @@ function objectSet(object, key, value) {
   return copy;
 }
 
+function getFieldTranslation(field) {
+  if (translations.prototype.hasOwnProperty.call(field)) return translations[field];
+  return field;
+}
+
+ // JavaScript does not have a type system, so we have to "add" our own
+const validItemFields = ["price", "quantity", "shipping", "tax"];
+
+// Translation tables are possible if we require a field name change later on
+const translations = { "quantity": "number" };
+
 // Set a field in an object
 function setFieldByName(cart, name, field, value) {
-  const item = cart[name];
-  const newItem = objectSet(item, field, value);
-  const newCart = objectSet(cart, name, newItem);
+  // Validate the field
+  if (!validItemFields.includes(field)) throw `Not a valid item field: '${field}'.`;
+  return objectSet(cart, name, objectSet(cart[name], getFieldTranslation(field), value));
+}
 
-  return newCart;
+const validIncrementFields = ["size", "quantity"];
+function incrementFieldByName(cart, name, field) {
+  // Validate the field
+  if (!validIncrementFields.includes(field)) throw `This field cannot be incremented: '${field}'.`;
+  
+  const item = cart[name];
+  
+  // Get field translation, if it exists
+  field = getFieldTranslation(field);
+  return objectSet(cart, name, objectSet(item, field, item[field] + 1));
 }
 
 const cart = objectSet({}, "thing", {});
-console.log(setFieldByName(cart, "thing", "price", "0.99"));
-console.log(setFieldByName(cart, "thing", "quantity", 1));
-console.log(setFieldByName(cart, "thing", "shipping", "Express"));
-console.log(setFieldByName(cart, "thing", "tax", "0.0495"));
+console.dir(setFieldByName(cart, "thing", "price", "0.99"));
+console.dir(setFieldByName(cart, "thing", "quantity", 1));
+console.dir(setFieldByName(cart, "thing", "shipping", "Express"));
+console.dir(setFieldByName(cart, "thing", "tax", "0.0495"));
+
+console.dir(incrementFieldByName(cart, "thing", "quantity"))
