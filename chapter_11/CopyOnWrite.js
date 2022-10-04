606b600fe873b35103976db653d4c73e4c85a074
Ryan Fickenscher <rsficken@gmail.com>
2022-10-03 20:57:15 -0700

    chapter 11, withArrayCopy

--- chapter_11/CopyOnWrite.js
+++ chapter_11/CopyOnWrite.js
@@ -2,31 +2,54 @@
 // to the `arraySet` function from chapter 6 using the new
 // function `withArrayCopy`
 function arraySet(array, idx, value) {
-  let copy = array.slice();
-  copy[idx] = value;
-  return copy;
+  return withArrayCopy(array, copy => copy[idx] = value );
 }
 
-function withArrayCopy() {
-
+function withArrayCopy(array, fn) {
+  const copy = array.slice();
+  fn(copy);
+  return copy;
 }
 
 // Apply the Replace Body with Callback Refactoring pattern
 // to the following "Copy On Write" functions
 function push(array, elem) {
-  let copy = array.slice();
-  copy.push(elem);
-  return copy;
+  return withArrayCopy(array, copy => copy.push(elem));
 }
 
 function dropLast(array) {
-  let copy = array.slice();
-  copy.pop();
-  return copy;
+  return withArrayCopy(array, copy => copy.pop());
 }
 
 function dropFirst(array) {
-  let copy = array.slice();
-  copy.shift();
+  return withArrayCopy(array, copy => copy.shift());
+}
+
+// Call functions
+console.log(arraySet([1, 2, 3], 1, 4)[1] === 4);
+console.log(push([1, 2, 3], 4)[3] === 4);
+console.log(dropLast([1, 2, 3])[1] === 2);
+console.log(dropFirst([1, 2, 3])[0] === 2);
+
+// Now it's your turn. Write a new fucntion `withObjectCopy`
+// and use it to re-implement these two object copy-on-write
+// funtions
+function withObjectCopy() {
+  
+}
+
+function objectSet(object, key, value) {
+  const copy = Object.assign({}, object);
+  copy[key] = value;
   return copy;
 }
+
+function objectDelete(object, key) {
+  const copy = Object.assign({}, object);
+  delete copy[key];
+  return copy;
+}
+
+// Call functions
+console.log(objectSet({}, "foo", "bar").foo === "bar");
+console.log(objectDelete({ apple: 1, orange: 2 }, "apple").apple === undefined);
