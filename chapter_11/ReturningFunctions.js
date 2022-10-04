c8a60f68dc4606cc5eb5748569be36d81d471aab
Ryan Fickenscher <rsficken@gmail.com>
2022-10-03 21:43:58 -0700

    chapter 11, return functions, IYT

--- chapter_11/ReturningFunctions.js
+++ chapter_11/ReturningFunctions.js
@@ -1,11 +1,34 @@
 // Replace Body with a Callback Pattern Solution
-function withLogging(fn) {
-  try {
-    fn();
-  } catch(error) {
-    logToSnapErrors(error);
+function wrapLogging(fn) {
+  return (arg) => {
+    try {
+      fn(arg);
+    } catch(error) {
+      console.err(`{ level: error, message: ${error} }`);
+    }
   }
 }
 
-withLogging(() => saveUserData(user));
-withLogging(() => fetchProduct(productId));
+const saveUserDataWithLogging = wrapLogging(saveUserData);
+const fetchProductWithLogging = wrapLogging(fetchProduct);
+
+saveUserDataWithLogging(user);
+fetchProductWithLogging(productId);
+
+// It's Your Turn p. 285
+// Write a function that transforms the function you pass it into a function that
+// catches and ignores all errors
+// If there is an error, just return `null`
+// Maket it work on functions of at least three (3) arguments
+
+// HINT: You normally ignore errors by wrapping code in a try/catch block and doing
+//       nothing in the catch
+
+function wrapIgnoreErrors() { }
+
+
+// It's Your Turn p. 286
+// Write a function called `makeAdder` that makes a function to add a number to
+// another number
+
+function makeAdder() { }
