c03968c9cca7658b079c1ee380e2d07de98234e7
Ryan Fickenscher <rsficken@gmail.com>
2022-10-13 21:48:40 -0700

    chapter 13, maxKeys

--- chapter_13/clarifying_chains.js
+++ chapter_13/clarifying_chains.js
@@ -31,18 +31,24 @@ function reduce(array, init, f) {
   return accum;
 }
 
+function maxKey(array, init, f) {
+  return reduce(array, init, function(biggestSoFar, element) {
+    if (f(biggestSoFar) > f(element)) {
+      return biggestSoFar;
+    } else {
+      return element;
+    }
+  });
+}
+
 function biggestPurchasesBestCustomers(customers) {
   const bestCustomers = filter(customers, function(customer) {
     return customer.purchases.length > 3;
   });
 
   const biggestPurchases = map(bestCustomers, function(customer) {
-    return reduce(customer.purchases, {total: 0}, function(biggestSoFar, purchase) {
-      if (biggestSoFar.total > purchase.total) {
-        return biggestSoFar;
-      } else {
-        return purchase;
-      }
+    return maxKey(customer.purchases, { total: 0 }, function(purchase) {
+      return purchase.total;
     });
   });
   return biggestPurchases;
